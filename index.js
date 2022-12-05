a934873d3bf019c73c5a4fe1cd705434d66ea127
urrikki <stampanona@gmail.com>
2022-12-05 19:40:41 +0100

    insert

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const dbo = require("./db/db");
 const app = express();
 const port = 4444;
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 dbo.connectToServer();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
 suite du code ici
 */
 
 app.get("/", function (req, res) {
-    res.send("Ok");
+    res.send("Hello world");
   });
+
 /* index.js code before... */
 app.get("/pokemon/list", function (req, res) {
     //on se connecte à la DB MongoDB
@@ -35,8 +39,33 @@ app.get("/pokemon/list", function (req, res) {
       
   });
 
+  app.get('/something', (req, res) => {
+    const color1 = req.query.color1;
+    const color2 = req.query.color2;
+    /* 
+    Traitement du code ensuite...
+    */
+})
+
+app.post('/pokemon/insert', jsonParser, (req, res) => {
+    const body = req.body;
+    const dbConnect = dbo.getDb();
+    console.log('Got body:', body);
+    var name = body.name
+    dbConnect
+        .collection("pokemonName")
+        .insertOne({
+            name:name
+        })
+    //on code ensuite l'insertion dans mongoDB, lisez la doc hehe !!
+    res.json("A bien étais ajouté");
+
+});
+
 app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
 
-
+/*
+node index.js
+*/
